3777833c59b54db4b103d0259d28dd9e453308ba
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2024-10-17 14:34:29 -0500

    added base TODOs and made a silly little splash page

---
+++ index.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import colors from 'colors';
+import inquirer from 'inquirer';
+
+//TODO: Create a fancy splash for the README maker to run at the start
+let splashMessage =
+`  
+   |*************************|    
+   |     ~README Builder~    |
+   |                         |
+    ************************* `
+console.log(colors.magenta(splashMessage));
+//TODO: a list of prompts from inquirer...
+
+//TODO: Prompt for Project Title (input)
+
+//TODO: Prompt for Decription (input)
+
+//TODO: Prompt for Installation (input)
+
+//TODO: Prompt for Usage (input)
+
+//TODO: Prompt for Contributing (input)
+
+//TODO: Prompt for Test (input)
+
+//TODO: Prompt for Project Licence (List)
+
+//TODO: Prompt for Github Username (input)
+
+//TODO: Prompt for Email (input)
+
