d75f46ff7d087058aa09b688a58ed69dcbf91b0c
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2024-10-18 21:49:31 -0500

    added in a lot of code. added most of the function to build the README file

--- index.js
+++ index.js
@@ -3,30 +3,141 @@ import colors from 'colors';
 import inquirer from 'inquirer';
 
 //TODO: Create a fancy splash for the README maker to run at the start
+const licenceDict = {
+    'Apache 2.0 License' : '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    'Creative Commons' : '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)',
+    'The MIT License' : '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+}
 let splashMessage =
 `  
    |*************************|    
    |     ~README Builder~    |
    |                         |
-    ************************* `
+    ************************* 
+    
+    `
 console.log(colors.magenta(splashMessage));
 //TODO: a list of prompts from inquirer...
+inquirer
+.prompt([
+    {
+        //TODO: Prompt for Project Title (input)
+        type: 'input',
+        message: colors.magenta('Please enter the ') + colors.yellow('Title of your Project:'),
+        name: 'projectTitle'
+    },
+    {
+        //TODO: Prompt for Decription (input)
+        type: 'input',
+        message: colors.magenta('Please enter ') +  colors.yellow('Project Description:'),
+        name: 'description'
+    },
+    {
+        //TODO: Prompt for Installation (input)
+        type: 'input',
+        message: colors.magenta('Please enter instructions for ') + colors.yellow('Installation:'),
+        name: 'installation'       
+    },
+    {
+        //TODO: Prompt for Usage (input)
+        type: 'input',
+        message: colors.magenta('Please enter instructions for ') + colors.yellow('Usage:'),
+        name: 'usage'      
+    },
+    {
+        //TODO: Prompt for Contributing (input)
+        type: 'input',
+        message: colors.magenta('Please add text for ') + colors.yellow('Contributing:'),
+        name: 'contributing'       
+    },
+    {
+        //TODO: Prompt for Tests (input)
+        type: 'input',
+        message: colors.magenta('Please add instructions for ') + colors.yellow('Tests:'),
+        name: 'tests'     
+    },
+    {
+        //TODO: Prompt for Project Licence (List)
+        type: 'list',
+        message: colors.magenta('Please choose a ') + colors.yellow('Licence ') + colors.magenta('from the following list:'),
+        name: 'licence',
+        choices: ["Apache 2.0 License","Creative Commons","The MIT License"]     
+    },
+    {
+        //TODO: Prompt for Github Username (input)
+        type: 'input',
+        message: colors.magenta('Please enter your ') + colors.yellow('Github Username:'),
+        name: 'gitHubUsername'
+    },
+    {
+        //TODO: Prompt for Email (input)
+        type: 'input',
+        message: colors.magenta('Please enter your ') + colors.yellow('Email Address:'),
+        name: 'email'
+    }
+])
+.then((response) => {
+    readmeBuild = `
+    #${response.projectTitle}
 
-//TODO: Prompt for Project Title (input)
+    ${licenceDict[response.licence]}
 
-//TODO: Prompt for Decription (input)
+    ##Description
+    ${response.description}
+    
+    ####Table of Contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[Contributors](#contributors)
+    *[Tests](#tests)
+    *[Licence](#licence)
+    *[Questions](#questions)
+    
+    
+    ##Installation
 
-//TODO: Prompt for Installation (input)
+    ${response.installation}
+    [Return to Top](#${readmeFormater(response.projectTitle)})
 
-//TODO: Prompt for Usage (input)
+    ##Usage
 
-//TODO: Prompt for Contributing (input)
+    ${response.usage}
+    [Return to Top](#${readmeFormater(response.projectTitle)})
 
-//TODO: Prompt for Test (input)
+    ##Contributors
 
-//TODO: Prompt for Project Licence (List)
+    ${response.contributing}
+    [Return to Top](#${readmeFormater(response.projectTitle)})
 
-//TODO: Prompt for Github Username (input)
+    ##Tests
 
-//TODO: Prompt for Email (input)
+    ${response.tests}
+    [Return to Top](#${readmeFormater(response.projectTitle)})
+
+    ##Licence
+
+    ${licenceDict[response.licence]}
+    This project is protected by the ${response.licence} licence
+
+    ##Questions
+
+    ###
+    `
+    fs.writeFile('README.md', readmeBuild, (err) =>
+  err ? console.error(err) : colors.green(console.log('Success!'))
+)
+    colors.green(console.log(response.projectTitle + " README.md complete."));
+})
+
+const readmeFormater = (unformatted) => {
+    return (unformatted.replaceAll(" ","-")).toLowerCase();
+}
+
+
+
+
+
+
+
+
 
